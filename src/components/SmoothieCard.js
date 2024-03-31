ff272ef15c8acd1c14f1684b677195dc5b5c920f
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-03-31 10:56:36 +0800

    Fetching each by own unique id, Added edit button with styling

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom"
+
+
+
 const SmoothieCard = ({smoothie}) => {
     return (
         <div className="smoothie-card">
@@ -5,6 +9,11 @@ const SmoothieCard = ({smoothie}) => {
             <p>{smoothie.method}</p>
             <div className="rating">
                 {smoothie.rating}
+            </div>
+            <div className="buttons">
+                <Link to={'/' + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
             </div>
         </div>
     

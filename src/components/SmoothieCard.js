d53d778600480c5dd673263697616e9194522b08
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-03-31 11:10:32 +0800

    Able to delete it into the database by each keys

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
 
 
+const SmoothieCard = ({ smoothie }) => {
 
-const SmoothieCard = ({smoothie}) => {
+    const handleDelete = async () => {
+        const { data, error } = await supabase
+        .from('smoothies')
+        .delete()
+        .eq('id', smoothie.id)
+        .select()
+
+        if (error) { 
+            console.log(error)
+        }
+
+        if (data) {
+            console.log(data)
+        }
+    }
+
     return (
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
@@ -14,6 +31,7 @@ const SmoothieCard = ({smoothie}) => {
                 <Link to={'/' + smoothie.id}>
                     <i className="material-icons">edit</i>
                 </Link>
+                <i className="material-icons" onClick={ handleDelete }>delete</i>
             </div>
         </div>
     
